12bfde64ee060b13f537d6adb4cc7628e97cd1ce
Edjhernandez <edjhernandez@gmail.com>
2023-12-20 21:03:15 -0300

    saving progress

--- redux/features/dataLoginSlice.ts
+++ redux/features/dataLoginSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import { dataInput } from '@/app/lib/definitions';
 
 const initialState = {
